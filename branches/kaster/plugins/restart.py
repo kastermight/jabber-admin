9728c0f43cad6bc19421c5f26d2f2e215a61f8f1
kastermight <kastermight@127dbaf5-5054-1b03-9d65-5b00c9e16cec>
2012-01-26 09:42:48 +0000

    some fix

--- branches/kaster/plugins/restart.py
+++ branches/kaster/plugins/restart.py
@@ -10,6 +10,6 @@ def run(bot,mess,mode='chat'):
 	import sys
 	bot.send(xmpp.Message(mess.getFrom(),bot.phrases['BOT_RS'],mode))
 	bot.online = 0
-	os.system('nohup python bot.py')
+	os.system('nohup python2.7 bot.py')
 	#13
 	sys.exit()
